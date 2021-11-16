db890d50d738adb4cb4f75a7dab5c41f2b003040
Chris Kelley <chriskelley85@gmail.com>
2021-11-16 17:40:19 -0600

    profile page

--- potluck-planner/src/index.js
+++ potluck-planner/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Profile from './Components/Profile'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Profile />
   </React.StrictMode>,
   document.getElementById('root')
 );
