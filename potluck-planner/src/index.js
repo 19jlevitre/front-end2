e40a4ed47422188ae7e8d548a2f7bcc90a7ae6d6
Bobby Gomez <bobbygomez@Bobbys-MacBook-Pro.local>
2021-11-16 04:58:30 -0600

    Sign Up Page created

--- potluck-planner/src/index.js
+++ potluck-planner/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Login from './Components/LogIn';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <Login />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
