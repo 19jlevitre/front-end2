00822339ec826c39caf2776b5fc40e6d690f8d72
Chris Kelley <chriskelley85@gmail.com>
2021-11-16 21:47:32 -0600

    profile update

--- potluck-planner/src/Components/Profile.js
+++ potluck-planner/src/Components/Profile.js
@@ -2,7 +2,20 @@ import React from "react";
 
 export default function Profile (){
     return(
-        <h2>Pending Invites</h2>
+        <div>
+            <div>
+            <h2>Welcome ____</h2>
+        </div>
+        <div>
+            <h2> Pending Invites</h2>
+        </div>
+        <div>
+            <h2> Confirmed Invites</h2>
+        </div>
+        </div>
+        
+
+
             
     )
 }
