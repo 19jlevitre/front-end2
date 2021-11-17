39d57088c3cea94316f950f8aa906cefdfee2bc2
OrdainedRat <marcusjuniusbrutus213@gmail.com>
2021-11-17 13:05:33 -0600

    difficulties with newEvent

--- potluck-planner/src/Components/ProfileBoxes/newEvent.js
+++ potluck-planner/src/Components/ProfileBoxes/newEvent.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const NewForm = styled.div`
+    display: none;
+`
+const showNewForm = styled.div`
+    display: show;
+`
+
 
 export default function NewEvent() {
 
+    const [ count, setCount ] = useState(0)
+    const [RealValue, setRealValue] = useState(NewForm)
+    
+    const checker = () => {
+        if(count === 1) {
+            setRealValue(showNewForm)
+          }
+           setCount(0)
+          return RealValue;
+          
+    }
+
+    const onClick = () => {
+        setCount(1)
+        console.log(count)
+        
+   }
+
     return(
         <div>
+            
             <label>
-                <button>+</button>
+                <button onClick={onClick}>+</button>
                 New Event
             </label>
+            <RealValue>
+                <h2>Hello</h2>
+            </RealValue>
+        
         </div>    
     )
 }
