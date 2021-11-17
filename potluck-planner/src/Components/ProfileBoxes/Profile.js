369449872c80f94ca24a9b06937b22e674e322d3
OrdainedRat <marcusjuniusbrutus213@gmail.com>
2021-11-17 12:30:55 -0600

    rendered basic html in flex display

--- potluck-planner/src/Components/ProfileBoxes/Profile.js
+++ potluck-planner/src/Components/ProfileBoxes/Profile.js
@@ -1,8 +1,38 @@
 import React from "react";
+import PendingBox from "./pendingBox";
+import NewEvent from "./newEvent";
+import ConfirmedBox from './confirmed';
+import styled from "styled-components";
+
+const ProfilePage = styled.div`
+    .content{
+        display: flex;
+        justify-content: space-between;
+    }
+    .left-events{
+        display: flex;
+        flex-direction: column;
+    }
+    h1{
+        text-align: center;
+    }
+
+`
+
 
 export default function Profile (){
     return(
-        <h2>Pending Invites</h2>
-            
+        <ProfilePage>
+            <h1> Welcome, User </h1>
+            <div className='content'>
+                <div className='left-events'>
+                    <PendingBox/>
+                    <ConfirmedBox/>
+                </div>
+                <div className='right-new'>
+                    <NewEvent/>
+                </div>
+            </div>   
+        </ProfilePage>  
     )
 }
---
+++ potluck-planner/src/Components/ProfileBoxes/newEvent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function NewEvent() {
+
+    return(
+        <div>
+            <label>
+                <button>+</button>
+                New Event
+            </label>
+        </div>    
+    )
+}
